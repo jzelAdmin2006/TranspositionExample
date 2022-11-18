44db5a183beaffd5635139d1f9d3cb7da4ce60b2
jzelAdmin <johannes_zeller@sluz.ch>
2022-11-18 20:26:44 +0100

    implement FenceCryptography

--- src/main/java/tech/bison/trainee2021/encryption/FenceCryptography.java
+++ src/main/java/tech/bison/trainee2021/encryption/FenceCryptography.java
@@ -2,16 +2,108 @@ package tech.bison.trainee2021.encryption;
 
 public class FenceCryptography implements Encryption {
 
+  private final String plainText;
+  private final String cipher;
+  private final int key;
+
+  public FenceCryptography(String plainText, int key) {
+    validateKey(key);
+    this.key = key;
+    this.plainText = plainText;
+    this.cipher = calculateCipher();
+  }
+
+  public void validateKey(int key) {
+    if (key <= 1) {
+      throw new IllegalArgumentException(
+          String.format("The encryption key %s is invalid. This encryption key must have a value of at least 2.", key));
+    }
+  }
+
+  private String calculateCipher() {
+    char[][] fence = createFence(plainText);
+    return assembleCipher(fence);
+  }
+
+  private String assembleCipher(char[][] fence) {
+    String cipher = "";
+    for (char[] row : fence) {
+      for (char character : row) {
+        if (character != Character.UNASSIGNED) {
+          cipher += character;
+        }
+      }
+    }
+    return cipher;
+  }
+
+  private char[][] createFence(String text) {
+    char[][] fence = createEmptyFence(text);
+    int rowIndex = 0;
+    for (int charIndex = 0; charIndex < text.length(); charIndex++) {
+      fence[rowIndex][charIndex] = text.charAt(charIndex);
+      if (fencePlankIsFacingDown(rowIndex, charIndex, fence)) {
+        rowIndex++;
+      } else {
+        rowIndex--;
+      }
+    }
+    return fence;
+  }
+
+  boolean fencePlankIsFacingDown(int rowIndex, int charIndex, char[][] fence) {
+    return rowIndex == 0 || fence[rowIndex - 1][charIndex - 1] != Character.UNASSIGNED && rowIndex + 1 < key;
+  }
+
+  private char[][] createEmptyFence(String text) {
+    char[][] fence = new char[key][];
+    for (int i = 0; i < fence.length; i++) {
+      fence[i] = new char[text.length()];
+    }
+    return fence;
+  }
+
+  public FenceCryptography(int key, String cipher) {
+    validateKey(key);
+    this.key = key;
+    this.cipher = cipher;
+    plainText = calculatePlainText();
+  }
+
+  private String assemblePlainText(char[][] fence) {
+    String plainText = "";
+    int rowIndex = 0;
+    for (int charIndex = 0; charIndex < cipher.length(); charIndex++) {
+      plainText += fence[rowIndex][charIndex];
+      if (fencePlankIsFacingDown(rowIndex, charIndex, fence)) {
+        rowIndex++;
+      } else {
+        rowIndex--;
+      }
+    }
+    return plainText;
+  }
+
+  private String calculatePlainText() {
+    char[][] fence = createFence(cipher);
+    int cipherCharIndex = 0;
+    for (int rowIndex = 0; rowIndex < key; rowIndex++) {
+      for (int charIndex = 0; charIndex < cipher.length(); charIndex++) {
+        if (fence[rowIndex][charIndex] != Character.UNASSIGNED) {
+          fence[rowIndex][charIndex] = cipher.charAt(cipherCharIndex++);
+        }
+      }
+    }
+    return assemblePlainText(fence);
+  }
+
   @Override
   public String getPlainText() {
-    // TODO Auto-generated method stub
-    return null;
+    return plainText;
   }
 
   @Override
   public String getCipher() {
-    // TODO Auto-generated method stub
-    return null;
+    return cipher;
   }
-
 }
--- src/test/java/tech/bison/trainee2021/encryption/FenceCryptographyTest.java
+++ src/test/java/tech/bison/trainee2021/encryption/FenceCryptographyTest.java
@@ -1,5 +1,140 @@
 package tech.bison.trainee2021.encryption;
 
-public class FenceCryptographyTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
+
+public class FenceCryptographyTest {
+  @Test
+  void newFenceCryptographyWithPlainText_getPlainText_isTheSame() {
+    FenceCryptography fenceCryptography = new FenceCryptography("super secret text", 2);
+
+    String result = fenceCryptography.getPlainText();
+
+    assertThat(result).isEqualTo("super secret text");
+  }
+
+  @Test
+  void newFenceCryptographyWithDifferentPlainText_getPlainText_isTheSame() {
+    FenceCryptography fenceCryptography = new FenceCryptography("super secret text version 2", 2);
+
+    String result = fenceCryptography.getPlainText();
+
+    assertThat(result).isEqualTo("super secret text version 2");
+  }
+
+  @Test
+  void newFenceCryptographyWithCipher_getCipher_isTheSame() {
+    FenceCryptography fenceCryptography = new FenceCryptography(2, "encrypted stuff");
+
+    String result = fenceCryptography.getCipher();
+
+    assertThat(result).isEqualTo("encrypted stuff");
+  }
+
+  @Test
+  void newFenceCryptographyWithDifferentCipher_getCipher_isTheSame() {
+    FenceCryptography fenceCryptography = new FenceCryptography(2, "encrypted stuff version 2");
+
+    String result = fenceCryptography.getCipher();
+
+    assertThat(result).isEqualTo("encrypted stuff version 2");
+  }
+
+  @Test
+  void newFenceCryptographyWithPlainText_getCipher_isCorrect() {
+    FenceCryptography fenceCryptography = new FenceCryptography("super secret text", 2);
+
+    String result = fenceCryptography.getCipher();
+
+    assertThat(result).isEqualTo("sprsce etue erttx");
+  }
+
+  @Test
+  void newFenceCryptographyWithDifferentPlainText_getCipher_isCorrect() {
+    FenceCryptography fenceCryptography = new FenceCryptography("super secret text version 2", 2);
+
+    String result = fenceCryptography.getCipher();
+
+    assertThat(result).isEqualTo("sprsce etvrin2ue erttx eso ");
+  }
+
+  @Test
+  void newFenceCryptographyWithCipher_getPlainText_isCorrect() {
+    FenceCryptography fenceCryptography = new FenceCryptography(2, "sprsce etue erttx");
+
+    String result = fenceCryptography.getPlainText();
+
+    assertThat(result).isEqualTo("super secret text");
+  }
+
+  @Test
+  void newFenceCryptographyWithDifferentCipher_getPlainText_isCorrect() {
+    FenceCryptography fenceCryptography = new FenceCryptography(2, "sprsce etvrin2ue erttx eso ");
+
+    String result = fenceCryptography.getPlainText();
+
+    assertThat(result).isEqualTo("super secret text version 2");
+  }
+
+  @Test
+  void newFenceCryptographyWithPlainTextAndDifferentKey_getCipher_isCorrect() {
+    FenceCryptography fenceCryptography = new FenceCryptography("super secret text", 8);
+
+    String result = fenceCryptography.getCipher();
+
+    assertThat(result).isEqualTo("seutxp tetre rsce");
+  }
+
+  @Test
+  void newFenceCryptographyWithDifferentPlainTextAndDifferentKey_getCipher_isCorrect() {
+    FenceCryptography fenceCryptography = new FenceCryptography("super secret text version 2", 8);
+
+    String result = fenceCryptography.getCipher();
+
+    assertThat(result).isEqualTo("seutxp t2et  revn reoscries");
+  }
+
+  @Test
+  void newFenceCryptographyWithCipherAndDifferentKey_getPlainText_isCorrect() {
+    FenceCryptography fenceCryptography = new FenceCryptography(5, "sctuerxpseee ttr ");
+
+    String result = fenceCryptography.getPlainText();
+
+    assertThat(result).isEqualTo("super secret text");
+  }
+
+  @Test
+  void newFenceCryptographyWithDifferentCipherAndDifferentKey_getPlainText_isCorrect() {
+    FenceCryptography fenceCryptography = new FenceCryptography(5, "sctnuerx o pseevi2e ttesr r");
+
+    String result = fenceCryptography.getPlainText();
+
+    assertThat(result).isEqualTo("super secret text version 2");
+  }
+
+  @Test
+  void newFenceCryptographyWithPlainTextAndInvalidKey_throwsException_isCorrectInstance() {
+    assertThatThrownBy(() -> new FenceCryptography(1, "")).isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("The encryption key 1 is invalid. This encryption key must have a value of at least 2.");
+  }
+
+  @Test
+  void newFenceCryptographyWithPlainTextAndDifferentInvalidKey_throwsException_isCorrectInstance() {
+    assertThatThrownBy(() -> new FenceCryptography(0, "")).isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("The encryption key 0 is invalid. This encryption key must have a value of at least 2.");
+  }
+
+  @Test
+  void newFenceCryptographyWithCipherAndInvalidKey_throwsException_isCorrectInstance() {
+    assertThatThrownBy(() -> new FenceCryptography("", 1)).isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("The encryption key 1 is invalid. This encryption key must have a value of at least 2.");
+  }
+
+  @Test
+  void newFenceCryptographyWithCipherAndDifferentInvalidKey_throwsException_isCorrectInstance() {
+    assertThatThrownBy(() -> new FenceCryptography("", 0)).isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("The encryption key 0 is invalid. This encryption key must have a value of at least 2.");
+  }
 }
